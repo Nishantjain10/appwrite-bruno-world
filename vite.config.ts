4f317633ac8f88f33d970737159e370f3641e3ba
Nishantjain10 <nishant.jain.775@gmail.com>
2024-02-02 18:47:29 +0530

    🐛fix chunk size limit

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,20 @@ import { glslify } from "vite-plugin-glslify";
 export default defineConfig({
   plugins: [react(), glslify()],
   assetsInclude: ["**/*.glb"],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: any) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1600,
+  },    
 });
